ff235bbc4a9ae544ab8c8141f4740a177e12eeb9
rmurtn <rmurtn@gmail.com>
2024-02-09 19:59:57 -0800

    added fillPracticeFormTest

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -4,11 +4,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.id;
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -17,17 +17,40 @@ public class PracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000;
     }
 
     @Test
     void fillPracticeFormTest() {
+
         open("/automation-practice-form");
+
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
         $("#firstName").setValue("Tom");
         $("#lastName").setValue("Smith");
         $("#userEmail").setValue("tom@smith.com");
-        $("input[id=gender-radio-1]").click();
+        $("label[for=gender-radio-1]").click();
+        $("#userNumber").setValue("1111111111");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("5");
+        $(".react-datepicker__year-select").selectOptionByValue("1960");
+        $(".react-datepicker__day--023").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("label[for=hobbies-checkbox-1]").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/images.jpeg"));
+        $("#currentAddress").setValue("111 Alpine St");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Panipat")).click();
+        $("#submit").click();
 
+        $(".modal-body").shouldHave(text("Tom Smith"), text("tom@smith.com")
+                , text("1111111111"), text("23 June,1960"), text("Maths, Computer Science")
+                , text("Sports"), text("images.jpeg"), text("111 Alpine St"), text("Haryana Panipat"));
     }
 }
